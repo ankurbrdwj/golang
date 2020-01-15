7f5e88b7574fb895f661bd1cff370ff9e2b41d01
Ankur Bhardwaj <ankurbhardwaj@xe-ggn-it-u1428.XEBIAINDIA.LOCAL>
2020-01-15 18:03:28 +0530

    enforce impementation

---
+++ goprimer/05_interfaces/main/main.go
@@ -0,0 +1,26 @@
+package main
+
+import(
+"fmt"
+)
+type Interface interface{
+Read()
+}
+
+type Struct struct{
+data int
+}
+
+func (s *struct) Read() {
+	fmt.Println("implemented the interface")
+}
+
+func main() {
+	ps := new(Struct)
+	pi := Interface(ps)
+
+	_, _ = pi, ps
+	
+fmt.Println("%v","%T",pi)
+fmt.Println("%v%T",ps)
+}
